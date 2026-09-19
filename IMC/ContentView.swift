d24e969e98264653a0f04b08b41aeee197f64dc6
Eric Brito <eric_brito@msn.com>
2026-09-19 08:34:26 -0300

    Initial Commit

---
+++ IMC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IMC
+//
+//  Created by Eric Alves Brito on 19/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IMC/IMCApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMCApp.swift
+//  IMC
+//
+//  Created by Eric Alves Brito on 19/09/26.
+//
+
+import SwiftUI
+
+@main
+struct IMCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
